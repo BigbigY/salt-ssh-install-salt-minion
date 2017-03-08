e76d1d4401aae3177de7e7279c399369bc46a7cf
wangyangyang <yy520it@163.com>
2017-03-08 22:35:05 +0800

    附脚本

---
+++ ip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USERNAME="root"
+PASSWORD="123"
+for i in `cat /root/host_ip.txt`
+do
+        echo "$i:" >> /etc/salt/roster ##$i表示取文件的每行内容
+        echo "  host: $i" >> /etc/salt/roster
+        echo "  user: $USERNAME" >>/etc/salt/roster
+        echo "  passwd: $PASSWORD" >>/etc/salt/roster
+#        echo "  sudo: True" >>/etc/salt/roster
+        echo "  timeout: 10" >>/etc/salt/roster
+done
