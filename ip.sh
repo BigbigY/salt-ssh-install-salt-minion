304d3bde159e15e93630c1a06af41f3c0106f225
wangyangyang <yy520it@163.com>
2017-03-12 15:20:08 +0800

    修改脚本路径

--- ip.sh
+++ ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 USERNAME="root"
 PASSWORD="123"
-for i in `cat /root/host_ip.txt`
+for i in `cat ./host_ip.txt`
 do
         echo "$i:" >> /etc/salt/roster ##$i表示取文件的每行内容
         echo "  host: $i" >> /etc/salt/roster
